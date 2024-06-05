f1d3ce25f630c984f63993621d374ece809d7aef
mzaky <mzakyf5503@gmail.com>
2024-06-05 10:41:17 +0700

    add book

--- src/main/java/zaky/driver/Driver.java
+++ src/main/java/zaky/driver/Driver.java
@@ -14,16 +14,17 @@ public class Driver {
         Buku buku1 = new Buku("Hari Poter", 5);
         Buku buku2 = new Buku("Setengah Koala", 3);
         Buku buku3 = new Buku("Literature", 2);
+        Buku buku4 = new Buku("Fiksi", 2);
 
         buku1.setKodeBuku("FIC0001");
         buku2.setKodeBuku("NFC0001");
         buku3.setKodeBuku("TXT0001");
-        
+
         DataPeminjaman dataPinjam = new DataPeminjaman("Zaky");
-        
-        Buku[] daftarBuku = {buku1, buku2, buku3};
+
+        Buku[] daftarBuku = { buku1, buku2, buku3 };
         dataPinjam.setDaftarBuku(daftarBuku);
-        
+
         dataPinjam.mulaiPinjamBuku_1302213067();
 
         dataPinjam.tampilkanDaftarBuku_1302213067();
